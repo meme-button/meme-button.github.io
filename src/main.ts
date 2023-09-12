695f9b736c44edbe339367ca686cf6878753ff92
Oliver Mak <oliver.mak@naxos.com>
2023-09-12 17:29:49 +0800

    CSS things

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-import "./assets/main.css";
-
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+
+// #region : CSS
+import "normalize.css";
+import "@/assets/css/variables.css";
+import "@/assets/css/transition.css";
+import "@/assets/css/main.css";
+//  #endregion
 
 const app = createApp(App);
 
