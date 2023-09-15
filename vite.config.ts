6473421f87016460732399b3f64eb7776df26bca
Oliver Mak <oliver.mak@naxos.com>
2023-09-15 12:54:25 +0800

    Drop console log and debugger on prod

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     vue(),
     AutoImport({
@@ -19,5 +19,9 @@ export default defineConfig({
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
     }
-  }
-});
+  },
+  esbuild: {
+    drop: mode === "production" ? ["debugger"] : [],
+    pure: mode === "production" ? ["console.log", "console.debug"] : [],
+  },
+}));
