aeea7d170b691dde54b7e1f427137a90e55d67fc
Oliver Mak <oliver139.working@gmail.com>
2023-10-02 19:55:02 +0800

    Layout

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,63 @@
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <div class="site-logo">
+      <img class="logo-normal" src="/img/meme-button-logo.svg" alt="めめボタンのロゴ">
+      <img class="logo-pressed" src="/img/meme-button-logo-hover.svg" alt="めめボタンのロゴ（押した）">
     </div>
   </header>
-
-  <RouterView />
+  <main>
+    <div class="content">
+    </div>
+  </main>
 </template>
 
 <script setup lang="ts">
 </script>
 
-<style scoped>
+<style lang="scss">
+#app {
+  display: flex;
+  flex-flow: column nowrap;
+  height: 100%;
+  max-width: 95%;
+  margin: auto;
+}
+</style>
+
+<style lang="scss" scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+  padding-top: 1.5rem;
+  padding-bottom: 0.75rem;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+.site-logo {
+  img {
+    width: 20rem;
+    &.logo-pressed {
+      display: none;
+    }
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
+  &:active {
+    .logo-normal {
+      display: none;
+    }
+    .logo-pressed {
+      display: block;
+    }
   }
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+main {
+  flex: 1 1 0;
+  padding-top: 0.75rem;
+  padding-bottom: 2rem;
+  overflow: hidden;
+}
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.content {
+  height: 100%;
+  background: rgba(255 255 255 / 65%);
+  border-radius: 1rem;
+  padding: 1.5rem;
+  overflow: auto;
 }
 </style>
