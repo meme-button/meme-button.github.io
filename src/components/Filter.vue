034183561ec6706b641929ab9384c3e0cb8fed2d
Oliver Mak <oliver139.working@gmail.com>
2023-10-08 22:54:26 +0800

    Filter widget

---
+++ src/components/Filter.vue
@@ -0,0 +1,71 @@
+<template>
+  <ul class="filter-list">
+    <li v-for="(option, index) in filterOptions" :key="option.value" :class="{ 'active': index === activeFilterIndex }">
+      <button type="button" @click="changefilter(option.value, index)">{{ option.name }}</button>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+export interface filterOptionStru {
+  name: string,
+  value: string|number,
+}
+</script>
+
+<script setup lang="ts">
+const props = defineProps<{
+  filterOptions: filterOptionStru[],
+}>();
+const emit = defineEmits<{
+  (e: "filterClicked", para: filterOptionStru["value"]): void
+}>();
+
+const activeFilterIndex = ref<number>();
+onBeforeMount(() => {
+  activeFilterIndex.value = 0;
+});
+
+function changefilter(option:filterOptionStru["value"], index:number) {
+  activeFilterIndex.value = index;
+  emit("filterClicked", option);
+}
+</script>
+
+<style lang="scss" scoped>
+.filter-list {
+  list-style: none;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-start;
+  align-items: center;
+  gap: .75rem;
+  padding: 0;
+  margin: 0;
+  li {
+    button {
+      position: relative;
+      bottom: 0;
+      background: #fff;
+      border-radius: 10rem;
+      padding: .5rem .75rem;
+      transition: all .3s ease;
+      &:hover, &:active {
+        color: #fff;
+        bottom: .25rem;
+        background: rgb(var(--color-theme1));
+        box-shadow: 0px .25rem .5rem rgb(var(--color-theme1-dark));
+      }
+    }
+    &.active {
+      button {
+        color: #fff;
+        font-weight: bold;
+        bottom: 0;
+        background: rgb(var(--color-theme1));
+        box-shadow: none;
+      }
+    }
+  }
+}
+</style>
--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -1,5 +1,11 @@
 <template>
   <section class="playgound">
+    <header>
+      <div class="widget widget-filter">
+        <span class="widget-label">フィルター</span>
+        <Filter :filter-options="filterOptions" />
+      </div>
+    </header>
     <ul class="button-list">
       <li v-for="sound in soundData" :key="sound.id">
         <SoundButton
@@ -12,14 +18,50 @@
 </template>
 
 <script setup lang="ts">
-import soundData from "@/data/sound";
+import type { filterOptionStru } from "@/components/Filter.vue";
+import soundData, { jpGroupOption, jpGroup } from "@/data/sound";
 
 const twoWeeksAgoDate = new Date();
 twoWeeksAgoDate.setDate(twoWeeksAgoDate.getDate() - 14);
+
+// #region : Filter options
+const filterOptions = computed(():filterOptionStru[] => {
+  const options:filterOptionStru[] = [];
+  for (const [key, value] of jpGroup.entries()) {
+    options.push({ name: value, value: key });
+  }
+  return [
+    { name: "全て", value: "All" },
+    ...options,
+    { name: "その他", value: "Others" },
+  ];
+});
+//  #endregion
 </script>
 
 <style lang="scss" scoped>
 .playgound {
+  header {
+    border-bottom: 1px solid rgb(var(--color-theme2-dark));
+    padding-bottom: 1rem;
+    margin-bottom: 1rem;
+  }
+  .widget {
+    .widget-label {
+      font-weight: 500;
+    }
+  }
+
+  .widget-filter {
+    display: flex;
+    flex-flow: row nowrap;
+    gap: 1rem;
+    .widget-label {
+      flex: 0 0 auto;
+      padding: .5rem 0;
+    }
+  }
+
   .button-list {
     list-style: none;
     display: flex;
